ff56cacf5889836cc049cc8375faf05d45e4b662
TACOWASA_1231 <98175856+TeamSquidTempura@users.noreply.github.com>
2023-06-08 00:58:51 +0900

    Add files via upload

--- game/clicker/clicker.js
+++ game/clicker/clicker.js
@@ -1,20 +1,28 @@
-var click_count = 0
 function clicker(){
-    click_count++
-    document.getElementById('count').innerHTML = click_count ;
-    if (click_count === 100) {
-        alert("congratulation you clicked 100 times")
-    }
-    if (click_count === 1000){
-        alert("congratulation you clicked 1000 times")
-    }
-    if (click_count === 10000){
-        alert("congratulation you clicked 10000 times")
-    }
-    if (click_count === 100000){
-        alert("congratulation you clicked 100000 times")
-    }
-    if (click_count === 1000000){
-        alert("congratulation you clicked 1000000 times")
-    }
+    const cookies = document.cookie;
+    const array = cookies.split(';');
+    let click_count = 0
+    array.forEach(function(value) {
+        const content = value.split('=');
+        console.log(content[1]);
+        click_count = Number(content[1]) + 1
+        document.cookie = "click_count="+click_count;
+        document.getElementById('count').innerHTML = click_count ;
+        if (click_count === 100) {
+            alert("congratulation you clicked 100 times")
+        }
+        if (click_count === 1000){
+            alert("congratulation you clicked 1000 times")
+        }
+        if (click_count === 10000){
+            alert("congratulation you clicked 10000 times")
+        }
+        if (click_count === 100000){
+            alert("congratulation you clicked 100000 times")
+        }
+        if (click_count === 1000000){
+            alert("congratulation you clicked 1000000 times")
+        }
+
+    })
 }
