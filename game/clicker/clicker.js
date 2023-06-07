bdf55ab15413690743b9e4408ca87316657c1bf1
TACOWASA_1231 <98175856+TeamSquidTempura@users.noreply.github.com>
2023-06-07 22:39:56 +0900

    Add files via upload

--- game/clicker/clicker.js
+++ game/clicker/clicker.js
@@ -1,8 +1,8 @@
 var click_count = 0
 function clicker(){
-    click_count = click_count + 1
+    click_count++
     document.getElementById('count').innerHTML = click_count ;
-    if (click_count === 100){
+    if (click_count === 100) {
         alert("congratulation you clicked 100 times")
     }
     if (click_count === 1000){
@@ -13,5 +13,8 @@ function clicker(){
     }
     if (click_count === 100000){
         alert("congratulation you clicked 100000 times")
+    }
+    if (click_count === 1000000){
+        alert("congratulation you clicked 1000000 times")
     }
 }
